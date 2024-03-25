0600b0cbb6b2f094ad32eef62dcc29b1595b1c3b
Matthew Jackson <103076064+matthewjackson202@users.noreply.github.com>
2024-03-25 15:05:06 -0400

    Add template project

---
+++ README.md
@@ -0,0 +1,1 @@
+UiPath Custom Activity Template Windows
---
+++ UiPath.Activity.Template.Windows/BaseDocumentationActivity.cs
@@ -0,0 +1,8 @@
+﻿using System.Activities;
+
+namespace UiPath.Activity.Template.Windows;
+
+public abstract class BaseTemplateActivity : CodeActivity
+{
+	protected override void Execute(CodeActivityContext context) { }
+}
